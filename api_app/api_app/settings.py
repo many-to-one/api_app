e785d67d40acbab61c12271a08076baf51a8970b
many-to-one <xllxlex@gmail.com>
2024-03-21 08:49:53 +0100

    base.html; header.html; static files;

--- api_app/api_app/settings.py
+++ api_app/api_app/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'api_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # connect 'templates' folder (base.html) from the project directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
