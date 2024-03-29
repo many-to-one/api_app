59649b539fee42c0cbd616b54abcbb281f51576c
many-to-one <xllxlex@gmail.com>
2024-03-29 09:20:55 +0100

    single order details

--- api_app/main/urls.py
+++ api_app/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from .views import *
-from .views_folder import errors_views
+from .views_folder import errors_views, orders_views
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('get_authorization_code', get_authorization_code, name='get_authorization_code'),
     path('get_access_token/<str:authorization_code>', get_access_token, name='get_access_token'),
     path('post_product', post_product, name='post_product'),
-    path('get_orders', get_orders, name='get_orders'),
-    path('get_orders', get_orders, name='get_orders'),
+    path('get_orders', orders_views.get_orders, name='get_orders'),
+    path('get_order_details/<uuid:id>', orders_views.get_order_details, name='get_order_details'),
     path('invalid_token', errors_views.invalid_token, name='invalid_token'),
 ] 
---
+++ api_app/main/views_folder/orders_views.py
@@ -0,0 +1,63 @@
+import json
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from ..utils import *
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+REDIRECT_URI = os.getenv('REDIRECT_URI')      # wprowadź redirect_uri
+AUTH_URL = os.getenv('AUTH_URL')
+TOKEN_URL = os.getenv('TOKEN_URL')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+
+def get_orders(request):
+
+    user = get_user(request)
+
+    try:
+        url = "https://api.allegro.pl.allegrosandbox.pl/order/checkout-forms"
+        # headers = {'Authorization': 'Bearer ' + token, 'Accept': "application/vnd.allegro.public.v1+json"}
+        headers = {'Authorization': f'Bearer {user.access_token}', 'Accept': "application/vnd.allegro.public.v1+json"}
+        product_result = requests.get(url, headers=headers, verify=True)
+        result = product_result.json()
+        if 'error' in result:
+            error_code = result['error']
+            if error_code == 'invalid_token':
+                print('ERROR RESULT @@@@@@@@@', error_code)
+                return redirect('invalid_token')
+
+        # print('RESULT @@@@@@@@@', json.dumps(result, indent=4))
+        context = {
+            'result': product_result.json()
+        }
+        return render(request, 'get_orders.html', context)
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+
+def get_order_details(request, id):
+
+    user = get_user(request)
+
+    try:
+        url = f"https://api.allegro.pl.allegrosandbox.pl/order/checkout-forms/{id}"
+        headers = {'Authorization': f'Bearer {user.access_token}', 'Accept': "application/vnd.allegro.public.v1+json"}
+        product_result = requests.get(url, headers=headers, verify=True)
+        result = product_result.json()
+        if 'error' in result:
+            error_code = result['error']
+            if error_code == 'invalid_token':
+                print('ERROR RESULT @@@@@@@@@', error_code)
+                return redirect('invalid_token')
+
+        print('RESULT @@@@@@@@@', json.dumps(result, indent=4))
+        context = {
+            'order': product_result.json()
+        }
+        return render(request, 'get_order_details.html', context)
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
