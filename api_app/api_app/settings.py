dbc0df59943998d2e0337ee02470f62b0ad64b11
many-to-one <xllxlex@gmail.com>
2024-10-28 20:40:15 +0100

    change the images sequence in gallery logic ok

--- api_app/api_app/settings.py
+++ api_app/api_app/settings.py
@@ -154,6 +154,11 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', )
 
+
+# Media files configuration
+MEDIA_URL = '/media/'  # URL prefix for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- api_app/api_app/urls.py
+++ api_app/api_app/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('users/', include('users.urls'),)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
